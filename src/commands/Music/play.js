cf2fc18caf53de5293b140b2f63e38bed9bfdec5
mrhaiydar-tech <mr.haiydar@gmail.com>
2026-08-28 10:35:58 +0300

    Change command name from 'play' to 'ش'

--- src/commands/Music/play.js
+++ src/commands/Music/play.js
@@ -6,7 +6,7 @@ export default {
     slashOnly: true,
     category: 'Music',
     data: new SlashCommandBuilder()
-        .setName('play')
+        .setName('ش')
         .setDescription('Play a song or add it to the queue')
         .addStringOption((opt) =>
             opt.setName('query').setDescription('Song name or URL').setRequired(true),
